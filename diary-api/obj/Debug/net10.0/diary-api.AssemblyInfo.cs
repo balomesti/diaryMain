23362ec3a462ebb627c25d54d0286f8a6d8e8a63
Your Name <your@email.com>
2026-03-25 09:20:22 +0800

    Remove obsolete database files from diary-api

--- diary-api/obj/Debug/net10.0/diary-api.AssemblyInfo.cs
+++ diary-api/obj/Debug/net10.0/diary-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diary-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605f146adf680c15a79d5edc3a0d0fcdda157c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461985dd7f49a12c9b00d7ece83132bf79f607c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("diary-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diary-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
